8bcff5d5e164553fa99d78c986c17482083318dd
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-11-11 13:19:31 +0000

    GUI Criada

---
+++ Projeto/src/GUI.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class GUI extends JFrame{
+    public GUI()  {
+        setTitle("IscTorrent");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
